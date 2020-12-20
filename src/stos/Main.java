11cacf4626820cf983571a5caea89f2a403cb72d
JNoganski <noganski.jakub@gmail.com>
2020-12-20 11:59:59 +0100

    Real Test Commit

--- src/stos/Main.java
+++ src/stos/Main.java
@@ -5,12 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
         Nawiasy nawiasy = new Nawiasy();
-        String dane = "( ( ) (( )) )";
+        String dane = "(  () (( )) )";
+        String dane2 = "()(()";
         if (nawiasy.sprawdzNawiasy(dane)){
-            System.out.println("Nawiasy zagnieżdzone prawidłowo");
+            System.out.println("Nawiasy (  () (( )) ) zagnieżdzone prawidłowo");
         }
         else{
-            System.out.println("Nawiasy zagnieżdżone nieprawidłowo");
+            System.out.println("Nawiasy (  () (( )) ) zagnieżdżone nieprawidłowo");
+        }
+        if (nawiasy.sprawdzNawiasy(dane2)){
+            System.out.println("Nawiasy ()(() zagnieżdzone prawidłowo");
+        }
+        else{
+            System.out.println("Nawiasy ()(() zagnieżdżone nieprawidłowo");
         }
 
 
--- src/stos/Nawiasy.java
+++ src/stos/Nawiasy.java
@@ -4,25 +4,27 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 //push test
 public class Nawiasy {
-     static boolean sprawdzNawiasy(String dane){
+    static boolean sprawdzNawiasy(String dane) {
         Deque<Character> stos = new ArrayDeque<>();
 
-        for(int i=0;i < dane.length();i++){
+        for (int i = 0; i < dane.length(); i++) {
             char a = dane.charAt(i);
 
-            if (a == '('){
+            if (a == '(') {
                 stos.push(a);
-                continue;
+            } else {
+                if (a == ')') {
+                    if (stos.isEmpty()) {
+                        return false;
+                    } else {
+                        if (stos.peek() == '(') {
+                            stos.pop();
+                        }
+                    }
+                }
             }
-        if (stos.isEmpty())
-            return false;
-        char sprawdz;
-        sprawdz = stos.pop();
-        if (sprawdz == '(')
-            return false;
-        }
-    return (stos.isEmpty());
-        }
 
+        }
+        return stos.isEmpty();
     }
-
+}
